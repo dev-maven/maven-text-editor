1b5d50c85cf99be828e91b2110195127b579775b
Oluwaferanmi Akindiilete <>
2023-02-24 19:23:05 +0100

    dependency update for deployment

--- ckeditor/README.md
+++ ckeditor/README.md
@@ -57,7 +57,7 @@ Follow the guides available on https://ckeditor.com/docs/ckeditor5/latest/framew
 ## FAQ
 | Where is the place to report bugs and feature requests?
 
-You can create an issue on https://github.com/ckeditor/ckeditor5/issues including the build id - `iq8qqbnxcau-d3y0ig4fhpia`. Make sure that the question / problem is unique, please look for a possibly asked questions in the search box. Duplicates will be closed.
+You can create an issue on https://github.com/ckeditor/ckeditor5/issues including the build id - `bi4jtt229ynm-22c76l6g8l0b`. Make sure that the question / problem is unique, please look for a possibly asked questions in the search box. Duplicates will be closed.
 
 | Where can I learn more about the CKEditor 5 framework?
 
--- ckeditor/src/ckeditor.js
+++ ckeditor/src/ckeditor.js
@@ -8,6 +8,7 @@ import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
 import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
@@ -27,6 +28,7 @@ Editor.builtinPlugins = [
 	Essentials,
 	Heading,
 	Image,
+	ImageResize,
 	ImageUpload,
 	Indent,
 	IndentBlock,
@@ -57,8 +59,7 @@ Editor.defaultConfig = {
 			'outdent',
 			'indent',
 			'|',
-			'mediaEmbed',
-			'|'
+			'mediaEmbed'
 		]
 	},
 	language: 'en'
